dca14a973700d0d6c2b914f0557d787cbcd829d2
Guilherme Rodrigues <guilherme.rodrigues@mhnet.local>
2026-07-16 17:16:21 -0300

    feat(pfp): tooltip added

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -1,11 +1,20 @@
 import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
 import PersonIcon from '@mui/icons-material/Person';
+import { use } from 'react';
 
 const ProfilePicture = ({ size = 'small', user = null }) => {
-    
+
     const sizes = { small: 32, medium: 64, large: 128 };
     if (!sizes[size]) throw new Error(`Tamanho inválido para foto de perfil, esperado: ${Object.keys(sizes).join(', ')}`);
-    const avatarSx = { width: sizes[size], height: sizes[size] };
+    const avatarSx = {
+        width: sizes[size],
+        height: sizes[size],
+        border: 'solid 2px lightgrey',
+        "&:hover": {
+            opacity: 0.7
+        }
+    };
 
     if (!user) {
         return (
@@ -16,9 +25,11 @@ const ProfilePicture = ({ size = 'small', user = null }) => {
     }
 
     return (
-        <Avatar alt={user.nome} src={user.src || ""} sx={avatarSx} >
-            {user?.nome ? user.nome.charAt(0).toUpperCase() : <PersonIcon />}
-        </Avatar>
+        <Tooltip title={`${user.nome} - ${user.username}`}>
+            <Avatar alt={user.nome} src={user.src || ""} sx={avatarSx} >
+                {user?.nome ? user.nome.charAt(0).toUpperCase() : <PersonIcon />}
+            </Avatar>
+        </Tooltip>
     );
 };
 
